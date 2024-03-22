738fd38120d3215c8fc230d07166d16e5c4a5efb
jtamu <gtamu.dip@gmail.com>
2024-03-22 22:33:01 +0900

    feat: add requests

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
-from chalice import Chalice
+import os
+from chalice import Chalice, Response
+import logging
+import requests
 
-app = Chalice(app_name='google_login_back')
+
+app = Chalice(app_name="google_login_back")
+app.log.setLevel(logging.DEBUG)
+
+CLIENT_ID = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
+CLIENT_SECRET = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
 
 
-@app.route('/')
+@app.route("/token")
 def index():
-    return {'hello': 'world'}
+    req = app.current_request.json_body or {}
+    if "code" not in req:
+        return Response(body={"message": "server error"}, status_code=500)
 
+    body = {
+        "code": req["code"],
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "redirect_uri": REDIRECT_URI,
+        "grant_type": "authorization_code",
+    }
+    res = requests.post("https://oauth2.googleapis.com/token", json=body)
+    app.log.info(res.json())
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    return {"hello": "world"}
