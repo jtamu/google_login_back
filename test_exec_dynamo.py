4d7ee35f065893a46b564abbe2a0144f21151e23
jtamu <gtamu.dip@gmail.com>
2024-09-07 15:25:23 +0900

    feat: test exec dynamo

---
+++ test_exec_dynamo.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+DB_ENDPOINT="http://dynamo-test:8000"
+
+sub_env = os.environ.copy()
+sub_env["DB_ENDPOINT"] = DB_ENDPOINT
+subprocess.run(args=["python", "create_table.py"], env=sub_env)
