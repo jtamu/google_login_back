f042bc78690b272fed7ab6735bcacd4a39bccf15
jtamu <gtamu.dip@gmail.com>
2024-09-14 23:49:39 +0900

    feat: 不要ファイル削除

--- get_request.py
+++
@@ -1,9 +0,0 @@
-import sys
-import urllib3
-
-http = urllib3.PoolManager()
-
-url = sys.argv[1]
-response = http.request('GET', url)
-
-print(response.data.decode('utf-8'))
--- test_exec_dynamo.py
+++
@@ -1,8 +0,0 @@
-# import os
-import subprocess
-
-# DB_ENDPOINT="http://dynamo-test:8000"
-
-# sub_env = os.environ.copy()
-# sub_env["DB_ENDPOINT"] = DB_ENDPOINT
-subprocess.run(args=["python", "create_table.py"])
